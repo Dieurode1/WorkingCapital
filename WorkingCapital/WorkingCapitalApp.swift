e9cb0afb120ba7780f329955cd2985004d476ab7
Dieurode1 <dieurodetassytv@gmail.com>
2025-03-31 09:57:23 -0400

    Initial Commit

---
+++ WorkingCapital/WorkingCapitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingCapitalApp.swift
+//  WorkingCapital
+//
+//  Created by ghola on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkingCapitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
